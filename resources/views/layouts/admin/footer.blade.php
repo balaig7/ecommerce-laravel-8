fee209dc6caa159c07b77b8b771bfbd1d2aecc8a
BalaGanesh I <balaig7@gmail.com>
2022-02-02 14:52:44 +0530

    Admin front page layouts separated

---
+++ resources/views/layouts/admin/footer.blade.php
@@ -0,0 +1,31 @@
+        </div>
+        <!-- END wrapper -->
+
+
+        <!-- jQuery  -->
+        <script src="{{asset('js/admin/jquery.min.js')}}"></script>
+        <script src="{{asset('js/admin/bootstrap.min.js')}}"></script>
+        <script src="{{asset('js/admin/modernizr.min.js')}}"></script>
+        <script src="{{asset('js/admin/detect.js')}}"></script>
+        <script src="{{asset('js/admin/fastclick.js')}}"></script>
+        <script src="{{asset('js/admin/jquery.slimscroll.js')}}"></script>
+        <script src="{{asset('js/admin/jquery.blockUI.js')}}"></script>
+        <script src="{{asset('js/admin/waves.js')}}"></script>
+        <script src="{{asset('js/admin/wow.min.js')}}"></script>
+        <script src="{{asset('js/admin/jquery.nicescroll.js')}}"></script>
+        <script src="{{asset('js/admin/jquery.scrollTo.min.js')}}"></script>
+
+        <script src="{{asset('js/admin/jquery.sparkline.min.js')}}"></script>
+
+        <!-- Datatables-->
+        <script src="{{asset('js/admin/jquery.dataTables.min.js')}}"></script>
+        <script src="{{asset('js/admin/dataTables.bootstrap.js')}}"></script>
+        <script src="{{asset('js/admin/dataTables.responsive.min.js')}}"></script>
+        <script src="{{asset('js/admin/responsive.bootstrap.min.js')}}"></script>
+
+        {{-- <script src="{{asset('js/admin/dashborad.js')}}"></script> --}}
+
+        <script src="{{asset('js/admin/app.js')}}"></script>
+
+    </body>
+</html>
